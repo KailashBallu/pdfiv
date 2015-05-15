0ccacd327a31c67294e977ee4883175dd5a785c0
Adrian Pietka <adrian@pietka.me>
2015-05-15 09:23:42 +0200

    OCR - second way to getting text from PDF files

--- README.md
+++ README.md
@@ -9,13 +9,33 @@ Friendly indexer and viewer of PDF files by web application.
 - Python
 - Windows
 
+### External tools
+
+- Xpdf http://www.foolabs.com/xpdf/
+- https://code.google.com/p/tesseract-ocr/
+
 ### Executing
 
+Converting PDF:
+
+Default way: PDF > PNG + HTML + Text from PDF
+
 ```bash
-python.exe -u scripts/convertpdf.py files/pdf files/target
-python.exe -u scripts/createstructureindex.py files/target
-python.exe -u scripts/updatesearchindex.py files/target files/target/structureindex.json
-python.exe -u scripts/download.py urlist.txt files/pdf
+$: python.exe -u scripts/convertpdf.py files/pdf files/target default
+```
+
+OCR way: PDF > PNG > Text from OCR
+
+```bash
+$: python.exe -u scripts/convertpdf.py files/pdf files/target default
+```
+
+Other tools
+
+```bash
+$: python.exe -u scripts/createmetadata.py files/target
+$: python.exe -u scripts/updatesearchindex.py files/target
+$: python.exe -u scripts/download.py urlist.txt files/pdf
 ```
 
 ## Author
--- scripts/convertpdf.py
+++ scripts/convertpdf.py
@@ -1,12 +1,13 @@
 import os, glob
 
-source_root_path = "files/pdf/"
-target_root_path = "files/target/"
+bin_pdftohtml = "bin\\xpdfbin-win-3.04\\bin64\\pdftohtml.exe"
+bin_pdftopng = "bin\\xpdfbin-win-3.04\\bin64\\pdftopng.exe"
+bin_pdftotext = "bin\\xpdfbin-win-3.04\\bin64\\pdftotext.exe"
+bin_tesseract_ocr = "bin\\tesseract-ocr-3.02-win32-portable\\tesseract.exe"
 
-bin_path = "bin\\xpdfbin-win-3.04\\bin64\\"
-bin_pdftohtml = "pdftohtml.exe"
-bin_pdftopng = "pdftopng.exe"
-bin_pdftotext = "pdftotext.exe"
+def get_png_files(directory):
+	print("get files from: {}*.png".format(directory))
+	return glob.glob("{}*.png".format(directory))
 
 def get_pdf_files(directory):
 	print("get files from: {}*.pdf".format(directory))
@@ -20,30 +21,65 @@ def setup_target_directory(target):
 		os.mkdir(os.path.join(target, "png"))
 
 def create_html_from_pdf(source, target):
-	cmd = "{}{} -q -r 200 \"{}\" \"{}\"".format(bin_path, bin_pdftohtml, source, target)
+	cmd = "{} -q -r 200 \"{}\" \"{}\"".format(bin_pdftohtml, source, target)
 	os.system(cmd)
 	
 def create_png_from_pdf(source, target):
-	cmd = "{}{} -q -r 200 \"{}\" \"{}\"".format(bin_path, bin_pdftopng, source, target)
+	cmd = "{} -q -r 200 \"{}\" \"{}\"".format(bin_pdftopng, source, target)
 	os.system(cmd)
 
 def create_text_from_pdf(source, target):
-	cmd = "{}{} -q -enc UTF-8 -raw \"{}\" \"{}\"".format(bin_path, bin_pdftotext, source, target)
+	cmd = "{} -q -enc UTF-8 -raw \"{}\" \"{}\"".format(bin_pdftotext, source, target)
 	os.system(cmd)
 
+def create_text_from_png(source, target):
+	target = os.path.join(target, os.path.basename(source).replace(".png", ""))
+	cmd = "{} \"{}\" \"{}\" -lang pol".format(bin_tesseract_ocr, source, target)
+	os.system(cmd)
+
+def workflow_default(source, target):
+    setup_target_directory(target)
+    create_html_from_pdf(source, os.path.join(target, "html/"))
+    create_png_from_pdf(source, os.path.join(target, "png", "page"))
+    create_text_from_pdf(source, os.path.join(target, "content.txt"))
+
+def workflow_ocr(source, target):
+    target_png = os.path.join(target, "png")
+
+    setup_target_directory(target)
+    create_png_from_pdf(source, target_png)
+
+    files = get_png_files(target_png)
+    for filepath in files:
+        create_text_from_png(filepath, target)
+
+def workflow(name, source, target):
+    to_execute = {
+        "default": workflow_default,
+        "ocr": workflow_ocr
+    }.get(name, "default")
+
+    to_execute(source, target)
+
 if __name__ == "__main__":
-	files = get_pdf_files(source_root_path)
-	for filepath in files:
-		filename = os.path.basename(filepath)
+    source_root_path = "files/pdf/Nowiny Raciborskie/"
+    target_root_path = "files/target/"
+    workflow_type = "ocr" #default
 
-		print("start: {}".format(filename))
+    if not os.path.exists(source_root_path):
+        print("source path does not exists: {}".format(source_root_path))
+        exit()
 
-		file_target = os.path.join(target_root_path, filename.replace(".pdf", ""))
-		file_source = os.path.join(source_root_path, filename)
+    if not os.path.exists(target_root_path):
+        print("target path does not exists: {}".format(target_root_path))
+        exit()
 
-		setup_target_directory(file_target)
-		create_html_from_pdf(file_source, os.path.join(file_target, "html/"))
-		create_png_from_pdf(file_source, os.path.join(file_target, "png", "page"))
-		create_text_from_pdf(file_source, os.path.join(file_target, "content.txt"))
+    files = get_pdf_files(source_root_path)
+    for file_path in files:
+        file_name = os.path.basename(file_path)
+        file_target = os.path.join(target_root_path, file_name.replace(".pdf", ""))
+        file_source = os.path.join(source_root_path, file_name)
 
-		print("finished: {}".format(filename))
+        print("start: {}".format(file_name))
+        workflow(workflow_type, file_source, file_target)
+        print("finished: {}".format(file_name))
